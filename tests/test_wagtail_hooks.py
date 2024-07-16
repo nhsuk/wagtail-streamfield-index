2d5cbd6aa358de74249338b4df9388ad52563951
Ross Clark <ross_clark@outlook.com>
2024-07-16 19:08:11 +0100

    linting

--- tests/test_wagtail_hooks.py
+++ tests/test_wagtail_hooks.py
@@ -9,9 +9,13 @@ from streamfieldindex.wagtail_hooks import IndexEntryAdminURLFinder
 
 @pytest.mark.django_db
 class TestIndexEntryAdminURLFinder(WagtailTestUtils):
-
     def test_construct_edit_url(self):
         finder = IndexEntryAdminURLFinder()
-        instance = streamfieldindex.models.IndexEntry(page=PageFactory(), block_type="text", block_value="Hello, world!", block_path="content",)
+        instance = streamfieldindex.models.IndexEntry(
+            page=PageFactory(),
+            block_type="text",
+            block_value="Hello, world!",
+            block_path="content",
+        )
         expected_url = reverse("wagtailadmin_pages:edit", args=[instance.page.id])
         assert finder.construct_edit_url(instance) == expected_url
